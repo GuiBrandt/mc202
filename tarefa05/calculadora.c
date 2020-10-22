cbc03a3a4233a96da5ad9a3f55876619198546e0
Guilherme G. Brandt <g235970@dac.unicamp.br>
2020-10-22 17:30:00 -0300

    Configura tarefa 5 no vscode

---
+++ tarefa05/calculadora.c
@@ -0,0 +1,15 @@
+/**
+ * @file calculadora.c
+ * @author Guilherme G. Brandt (235970)
+ * 
+ * @see https://www.ic.unicamp.br/~lehilton/mc202ab/tarefas/tarefa05.html 
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <assert.h>
+
+int main() {
+    return 0;
+}
