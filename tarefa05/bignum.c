12fb2a610a9c35268f96d04383628d65fb0f7f96
Guilherme G. Brandt <g235970@dac.unicamp.br>
2020-10-23 01:41:00 -0300

    Refatoração e multiplicação

--- tarefa05/bignum.c
+++ tarefa05/bignum.c
@@ -96,6 +96,140 @@ void assert_valid(const bignum* ptr) {
 }
 #endif
 
+result_code add_with_carry(node_ptr current_lhs, bignum_item n) {
+    // Esta soma sempre é segura desde que N obedeça os limites de tamanho.
+    current_lhs->data += n;
+
+    // Por outro lado, temos que considerar que podemos estourar o limite
+    // e temos que "carregar" o que sobrou para os próximos nós.
+    node_ptr current_carry = current_lhs;
+    while (current_carry->data >= ITEM_MAX) {
+
+        int carry = current_carry->data / ITEM_MAX;
+        current_carry->data %= ITEM_MAX;
+
+        // Criamos um novo nó caso não tenhamos o suficiente
+        if (current_carry->next == NULL) {
+            current_carry->next = new_node();
+            if (current_carry->next == NULL)
+                return FAIL_OOM;
+        }
+
+        current_carry->next->data += carry;
+        current_carry = current_carry->next;
+    }
+
+    return SUCCESS;
+}
+
+result_code add_at_node(node_ptr current_lhs, const bignum* rhs) {
+    node_ptr current_rhs = rhs->internal;
+
+    while (current_rhs) {
+        bignum_item n = current_rhs->data;
+        result_code result;
+
+        // Essa soma sempre é segura, pois garantimos que os dados são menores
+        // que 10^9, e portanto sua soma sempre cabe no tipo inteiro usado.
+        if ((result = add_with_carry(current_lhs, n)) != SUCCESS)
+            return result;
+
+        current_rhs = current_rhs->next;
+        if (current_rhs == NULL)
+            break;
+
+        if (current_lhs->next == NULL) {
+            current_lhs->next = new_node();
+
+            if (current_lhs->next == NULL) {
+                return FAIL_OOM;
+            }
+        }
+
+        current_lhs = current_lhs->next;
+    }
+
+    return SUCCESS;
+}
+
+// Implementação de subtração assumindo que lhs >= rhs.
+result_code subtract_base(bignum* lhs, const bignum* rhs) {
+    node_ptr current_lhs = lhs->internal,
+             current_rhs = rhs->internal;
+
+    while (current_rhs) {
+
+        // Se o lado direito é maior que o esquerdo, temos que "emprestar" um
+        // valor das casas acima para fazer a subtração.
+        if (current_rhs->data > current_lhs->data) {
+
+            current_lhs->data += ITEM_MAX;
+            current_lhs->data -= current_rhs->data;
+
+            // Procuramos um valor não nulo acima e replicamos o passo de
+            // "emprestar" para cada nó no caminho, até encontrarmos um que
+            // possamos decrementar.
+            node_ptr current_carry = current_lhs->next,
+                     prev = current_lhs;
+
+            while (current_carry->data == 0) {
+                current_carry->data = ITEM_MAX - 1;
+                prev = current_carry;
+                current_carry = current_carry->next;
+            }
+
+            // Se formos deixar algum zero à esquerda, liberamos o nó
+            // correspondente.
+            if (current_carry->data == 1 && current_carry->next == NULL) {
+                prev->next = NULL;
+                free(current_carry);
+            } else {
+                current_carry->data--;
+            }
+
+        // Se não, podemos simplesmente subtrair e tudo fica bem
+        } else {
+            current_lhs->data -= current_rhs->data;
+        }
+
+        current_lhs = current_lhs->next;
+        current_rhs = current_rhs->next;
+    }
+
+    return SUCCESS;
+}
+
+// Multiplica um bignum por um único nó, e soma o resultado a um auxiliar
+result_code multiply_partial(
+    node_ptr fixed,
+    node_ptr current_aux,
+    const bignum* lhs
+) {
+    result_code result;
+
+    // Criamos mais um bignum auxiliar, dessa vez para armazenar os
+    // produtos parciais referentes à multiplicação de cada dígito em rhs
+    // pelo valor completo de lhs.
+    bignum summand;
+    if ((result = bignum_init(&summand)) != SUCCESS)
+        return result;
+
+    for (
+        node_ptr current_lhs = lhs->internal,
+                    current_summand = summand.internal;
+        current_lhs != NULL;
+        current_lhs = current_lhs->next,
+        current_summand = current_summand->next
+    ) {
+        bignum_item product = current_lhs->data * fixed->data;
+        add_with_carry(current_summand, product);
+    }
+
+    // Então somamos o produto parcial ao total e nos livramos dele
+    add_at_node(current_aux, &summand);
+    bignum_destroy(&summand);
+}
+
 //============================================================================
 // Implementações (Contrato)
 //============================================================================
@@ -261,85 +395,8 @@ result_code bignum_add(bignum* lhs, const bignum* rhs) {
     assert_valid(lhs);
     assert_valid(rhs);
 
-    node_ptr current_lhs = lhs->internal,
-             current_rhs = rhs->internal;
-
-    while (current_rhs) {
-        // Essa soma sempre é segura, pois garantimos que os dados são menores
-        // que 10^9, e portanto sua soma sempre cabe no tipo inteiro usado.
-        current_lhs->data += current_rhs->data;
-
-        // Por outro lado, temos que considerar que podemos estourar o limite
-        // e temos que "carregar" o que sobrou para os próximos nós.
-        node_ptr current_carry = current_lhs;
-        while (current_carry->data >= ITEM_MAX) {
-
-            int carry = current_carry->data / ITEM_MAX;
-            current_carry->data %= ITEM_MAX;
-
-            // Criamos um novo nó caso não tenhamos o suficiente
-            if (current_carry->next == NULL) {
-                current_carry->next = new_node();
-                if (current_carry->next == NULL)
-                    return FAIL_OOM;
-            }
-
-            current_carry->next->data += carry;
-            current_carry = current_carry->next;
-        }
-
-        current_lhs = current_lhs->next;
-        current_rhs = current_rhs->next;
-    }
-
-    return SUCCESS;
-}
-
-// Implementação de subtração assumindo que lhs >= rhs.
-result_code bignum_subtract_base(bignum* lhs, const bignum* rhs) {
-    node_ptr current_lhs = lhs->internal,
-             current_rhs = rhs->internal;
-
-    while (current_rhs) {
-
-        // Se o lado direito é maior que o esquerdo, temos que "emprestar" um
-        // valor das casas acima para fazer a subtração.
-        if (current_rhs->data > current_lhs->data) {
-
-            current_lhs->data += ITEM_MAX;
-            current_lhs->data -= current_rhs->data;
-
-            // Procuramos um valor não nulo acima e replicamos o passo de
-            // "emprestar" para cada nó no caminho, até encontrarmos um que
-            // possamos decrementar.
-            node_ptr current_carry = current_lhs->next,
-                     prev = current_lhs;
-
-            while (current_carry->data == 0) {
-                current_carry->data = ITEM_MAX - 1;
-                prev = current_carry;
-                current_carry = current_carry->next;
-            }
-
-            // Se formos deixar algum zero à esquerda, liberamos o nó
-            // correspondente.
-            if (current_carry->data == 1 && current_carry->next == NULL) {
-                prev->next = NULL;
-                free(current_carry);
-            } else {
-                current_carry->data--;
-            }
-
-        // Se não, podemos simplesmente subtrair e tudo fica bem
-        } else {
-            current_lhs->data -= current_rhs->data;
-        }
-
-        current_lhs = current_lhs->next;
-        current_rhs = current_rhs->next;
-    }
-
-    return SUCCESS;
+    node_ptr current_lhs = lhs->internal;
+    return add_at_node(current_lhs, rhs);
 }
 
 result_code bignum_subtract(bignum* lhs, const bignum* rhs) {
@@ -365,15 +422,53 @@ result_code bignum_subtract(bignum* lhs, const bignum* rhs) {
             lhs->internal = aux.internal;
             aux.internal = swap;
 
-            result = bignum_subtract_base(lhs, &aux);
+            result = subtract_base(lhs, &aux);
         }
 
         bignum_destroy(&aux);
         return result;
 
     } else {
-        return bignum_subtract_base(lhs, rhs);
+        return subtract_base(lhs, rhs);
     }
+}
+
+result_code bignum_multiply(bignum* lhs, const bignum* rhs) {
+    assert_valid(lhs);
+    assert_valid(rhs);
+
+    // Cria um bignum auxiliar para receber o resultado sem destruir o lado
+    // esquerdo.
+    bignum aux;
+
+    result_code result;
+    if ((result = bignum_init(&aux)) != SUCCESS)
+        return result;
+
+    // Percorremos para elemento do lado direito para multiplicar pelos
+    // elementos do lado esquerdo.
+    // Também avançamos na lista do auxiliar ao mesmo tempo, equivalente à
+    // multiplicação pelas potências de 10 que fazemos quando aplicamos o
+    // algoritmo da multiplicação conforme multiplicamos por casas decimais
+    // maiores.
+    for (
+        node_ptr current_rhs = rhs->internal, current_aux = aux.internal;
+        current_rhs != NULL;
+        current_rhs = current_rhs->next, current_aux = current_aux->next
+    ) {
+        result = multiply_partial(current_rhs, current_aux, lhs);
+
+        if (result != SUCCESS)
+            return result;
+    }
+
+    // Ao final, trocamos o auxiliar com o resultado e o lhs
+    node_ptr swap = lhs->internal;
+    lhs->internal = aux.internal;
+    aux.internal = swap;
+    
+    bignum_destroy(&aux);
+    return SUCCESS;
 }
 
 #include <stdio.h>
@@ -383,8 +478,8 @@ int main() {
     bignum_init(&big1);
     bignum_init(&big2);
 
-    bignum_parse(&big1, "1");
-    bignum_parse(&big2, "12345678910111213141516");
+    bignum_parse(&big1, "385641209744002900063486");
+    bignum_parse(&big2, "15136511244937992393588");
 
     node_ptr current = big1.internal;
     while (current) {
@@ -392,8 +487,6 @@ int main() {
         current = current->next;
     }
     printf("<END>\n");
-
-    bignum_subtract(&big2, &big1);
     
     current = big2.internal;
     while (current) {
@@ -402,6 +495,8 @@ int main() {
     }
     printf("<END>\n");
 
+    bignum_multiply(&big2, &big1);
+
     current = big2.internal;
     while (current) {
         printf("%llu -> ", current->data);
@@ -409,11 +504,11 @@ int main() {
     }
     printf("<END>\n");
 
-    char str[24];
-    bignum_sprintf(str, 24, &big1);
+    char str[256];
+    bignum_sprintf(str, 64, &big1);
     printf("%s ", str);
 
-    bignum_sprintf(str, 24, &big2);
+    bignum_sprintf(str, 256, &big2);
     printf("%s\n", str);
 
     bignum_destroy(&big1);
