2d119b96b5900ccd1169d3bbdbbb8ed808bb897f
Guilherme G. Brandt <g235970@dac.unicamp.br>
2020-10-26 13:33:50 -0300

    Implementação básica de deque

---
+++ tarefa06/deque.c
@@ -0,0 +1,121 @@
+#include "deque.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct _node {
+    void* elem;
+    struct _node* prev;
+    struct _node* next;
+} node_t;
+
+struct _deque {
+    node_t* head;
+    node_t* tail;
+};
+
+node_t* make_node(void* elem, node_t* prev, node_t* next) {
+    node_t* node = (node_t*) malloc(sizeof(node_t));
+
+    if (node == NULL) {
+        fprintf(stderr, "Out of memory");
+        exit(-1);
+    }
+
+    node->elem = elem;
+    node->prev = prev;
+    node->next = next;
+
+    return node;
+}
+
+deque* make_deque() {
+    return (deque*) calloc(1, sizeof(deque));
+}
+
+void destroy_deque(deque* q) {
+    assert(q != NULL);
+
+    node_t* it = q->head;
+
+    while (it != NULL) {
+        node_t* aux = it;
+        it = it->next;
+        free(aux);
+    }
+    
+    free(q);
+}
+
+int is_empty(deque* q) {
+    assert(q != NULL);
+
+    return q->head != NULL;
+}
+
+void push_back(deque* q, deque_elem_t elem) {
+    assert(q != NULL);
+    assert(q->tail != NULL);
+    assert(q->tail->next == NULL);
+
+    node_t* node = make_node(elem, q->tail, NULL);
+    q->tail->next = node;
+    q->tail = node;
+}
+
+void push_front(deque* q, deque_elem_t elem) {
+    assert(q != NULL);
+    assert(q->head != NULL);
+    assert(q->head->prev == NULL);
+
+    node_t* node = make_node(elem, NULL, q->head);
+    q->head->prev = node;
+    q->head = node;
+}
+
+deque_elem_t front(deque* q) {
+    assert(q != NULL);
+    assert(q->head != NULL);
+
+    return q->head->elem;
+}
+
+deque_elem_t back(deque* q) {
+    assert(q != NULL);
+    assert(q->tail != NULL);
+
+    return q->tail->elem;
+}
+
+void pop_back(deque* q) {
+    assert(q != NULL);
+    assert(q->tail != NULL);
+
+    node_t* tail = q->tail;
+    q->tail = tail->prev;
+ 
+    if (q->tail == NULL) { // Fila ficou vazia
+        q->head = NULL;
+
+    } else {
+        q->tail->next = NULL;
+    }
+
+    free(tail);
+}
+
+void pop_front(deque* q) {
+    assert(q != NULL);
+    assert(q->head != NULL);
+
+    node_t* head = q->head;
+    q->head = head->prev;
+
+    if (q->head == NULL) { // Fila ficou vazia
+        q->tail = NULL;
+    } else {
+        q->head->prev = NULL;
+    }
+
+    free(head);
+}
---
+++ tarefa06/deque.h
@@ -0,0 +1,101 @@
+/**
+ * @file deque.h
+ * @author Guilherme G. Brandt (235970)
+ * 
+ * @brief Definições do ADT de Deque.
+ */
+
+#ifndef __DEQUE_H__
+#define __DEQUE_H__
+
+/**
+ * @brief Estrutura opaca para um deque.
+ */
+typedef struct _deque deque;
+
+/**
+ * @brief Tipo para elementos de um deque.
+ */
+typedef void* deque_elem_t;
+
+/**
+ * @brief Constrói um deque vazio.
+ * 
+ * @return ponteiro para um deque
+ */
+deque* make_deque()
+    __attribute__((malloc));
+
+/**
+ * @brief Libera os recursos alocados para um deque.
+ * 
+ * @note Essa função não libera os recursos dos elementos do deque; isso deve
+ *       ser feito pelo usuário, seguindo o princípio de que a responsabilidade
+ *       por liberar um ponteiro recai sobre quem o aloca. 
+ * 
+ * @param q o deque.
+ * 
+ */
+void destroy_deque(deque* q);
+
+/**
+ * @brief Verifica se uma fila está vazia.
+ * 
+ * @param q a fila.
+ * @return true se estiver vazia, false caso contrário.
+ */
+int is_empty(deque* q);
+
+/**
+ * @brief Adiciona um elemento no fim da fila.
+ * 
+ * @param q a fila.
+ * @param elem o elemento.
+ */
+void push_back(deque* q, deque_elem_t elem);
+
+/**
+ * @brief Adiciona um elemento no começo da fila.
+ * 
+ * @param q a fila.
+ * @param elem o elemento.
+ */
+void push_front(deque* q, deque_elem_t elem);
+
+/**
+ * @brief Retorna o primeiro elemento da fila.
+ * 
+ * @note Essa função assume que a fila não está vazia. Seu comportamento é
+ *       indefinido caso chamada em uma fila vazia.
+ * 
+ * @param q a fila.
+ * @return o elemento.
+ */
+deque_elem_t front(deque* q);
+
+/**
+ * @brief Retorna o último elemento da fila.
+ * 
+ * @note Essa função assume que a fila não está vazia. Seu comportamento é
+ *       indefinido caso chamada em uma fila vazia.
+ * 
+ * @param q a fila.
+ * @return o elemento. 
+ */
+deque_elem_t back(deque* q);
+
+/**
+ * @brief Remove um elemento do começo da fila.
+ * 
+ * @param q a fila.
+ */
+void pop_back(deque* q);
+
+/**
+ * @brief Remove um elemento do começo da fila.
+ * 
+ * @param q a fila.
+ */
+void pop_front(deque* q);
+
+#endif // __DEQUE_H__
