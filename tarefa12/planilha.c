fa7994f89377ede1c9809e06914c45ac7c05b6a4
Guilherme G. Brandt <g235970@dac.unicamp.br>
2020-12-20 23:11:21 -0300

    Comentarios e organizaçao de novo

--- tarefa12/planilha.c
+++ tarefa12/planilha.c
@@ -1,3 +1,12 @@
+/**
+ * @file planilha.c
+ * @author Guilherme G. Brandt (235970)
+ * 
+ * @brief Implementação da lógica da tarefa.
+ * 
+ * @see https://www.ic.unicamp.br/~lehilton/mc202ab/tarefas/tarefa12.html 
+ */
+
 #include <stddef.h>
 #include <stdio.h>
 
@@ -6,10 +15,12 @@
 
 #include "spreadsheet.h"
 
+void get(spreadsheet_t* spreadsheet, char col, size_t row);
+void set(spreadsheet_t* spreadsheet, char col, size_t row, int value);
+
 int main() {
     char filename[128];
     size_t columns, rows;
-
     scanf("%s %lu %lu ", filename, &columns, &rows);
 
     FILE* csv = fopen(filename, "r");
@@ -23,32 +34,41 @@ int main() {
     int read;
     while ((read = scanf("%c %c%lu ", &op, &col, &row)) > 0) {
         if (op == 'G') {
-            printf("%c%lu: ", col, row);
+            get(spreadsheet, col, row);
 
-            int value = spreadsheet_eval(spreadsheet, col, row);
-            
-            if (value != INT_MIN) {
-                printf("%d", value);
-            } else {
-                printf("#ERRO#");
-            }
-
-            printf("\n");
         } else {
             assert(op == 'S');
 
             int value;
             scanf("%d ", &value);
-
-            int old = spreadsheet_eval(spreadsheet, col, row);
-
-            spreadsheet_update(spreadsheet, col, row, value);
-
-            printf("%c%lu: %d -> %d\n", col, row, old, value);
+            set(spreadsheet, col, row, value);
         }
     }
 
     destroy_spreadsheet(spreadsheet);
 
     return 0;
+}
+
+void get(spreadsheet_t* spreadsheet, char col, size_t row) {
+    printf("%c%lu: ", col, row);
+
+    int value = spreadsheet_eval(spreadsheet, col, row);
+    
+    if (value != INT_MIN) {
+        printf("%d", value);
+    } else {
+        printf("#ERRO#");
+    }
+
+    printf("\n");
+}
+
+void set(spreadsheet_t* spreadsheet, char col, size_t row, int value) {
+
+    int old = spreadsheet_eval(spreadsheet, col, row);
+
+    spreadsheet_update(spreadsheet, col, row, value);
+
+    printf("%c%lu: %d -> %d\n", col, row, old, value);
 }
